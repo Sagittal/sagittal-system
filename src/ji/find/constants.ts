2817acd2ad083e77f5529d69a3206b24d8dabf67
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-07 14:03:59 -0800

    removing to-dos that are no longer problems

--- src/ji/find/constants.ts
+++ src/ji/find/constants.ts
@@ -9,17 +9,6 @@ const DEFAULT_ZONE = {
     extrema: [DEFAULT_LOWER_BOUND, DEFAULT_UPPER_BOUND],
     exclusive: DEFAULT_EXCLUSIVE,
 } as Zone
-// Todo: now that I turned off that error about losing precision on the quotients,
-//  I think I'm starting to hit the descendant error more, the one where you e.g.
-//  Error: This integer 1862759518692539 contains primes which are too big; remainder is 235583599177
-//  And so I'm maybe regretting that...
-//  I reduced the likelihood of it by putting some try catches around places where it often happens
-//  Where it's not so important, such as computing comma names
-
-// Todo: figure out why --max-ate didn't override this...
-//  It makes sense that Infinity can't work because that's how it iterates through all the 3-exponents (w/ a Range),
-//  But you should at least be able to set it to something
-//  To be clear, what happened was I tried to pass max ATE but it somehow was still reported to be 20 (and was, I think)
 const DEFAULT_MAX_ATE = 20 as Max<Ate>
 const DEFAULT_MAX_AAS = 20 as Max<Abs<ApotomeSlope>>
 const DEFAULT_MAX_N2D3P9 = MAX_N2D3P9_FOR_WHICH_POSSIBLE_NUMERATORS_ARE_KNOWN
