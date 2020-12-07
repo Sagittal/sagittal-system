9adba7e547b70d43879dce80a48445b48ae797c7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-06 16:14:49 -0800

    now need to be able to run test-ci-names

--- spec/helpers/shared/reporters/specNameReporter.ts
+++ spec/helpers/shared/reporters/specNameReporter.ts
@@ -3,7 +3,7 @@ import {CustomReporterResult} from "jasmine-spec-reporter/built/spec-reporter"
 
 // This is quite handy when the suite starts to hang, so you can identify where the issue is.
 
-const PRINT_NAMES = process.argv[2] === "--names=true"
+const PRINT_NAMES = process.argv[3] === "--names=true"
 
 const specNameReporter: jasmine.CustomReporter = {
     specStarted(result: CustomReporterResult): void {
