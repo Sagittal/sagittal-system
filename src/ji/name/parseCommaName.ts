7b46a1f3292473806d75e412adb3ff9d2816e6f0
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-10 13:47:03 -0800

    new to-do, woo

--- src/ji/name/parseCommaName.ts
+++ src/ji/name/parseCommaName.ts
@@ -10,6 +10,13 @@ import {
 import {SIZE_CATEGORY_ABBREVIATIONS, SIZE_CATEGORY_NAMES, SIZE_CATEGORY_NAME_ALTERNATIVES} from "./sizeCategories"
 import {CommaNameQuotient, ParsedCommaName, SizeCategory, SizeCategoryName} from "./types"
 
+// TODO: comma names need to be updated to reflect the new understanding about up & down being overrides while
+//  The direction of the ratio in the comma name expresses the direction of the 2,3-free prime content as well as
+//  The direction of the comma itself (positive or negative cents)
+//  Which is made possible in part by a resolution to the counterexample given where two commas could have the same name
+//  Which I fixed by devising a series of tie-breakers
+//  See: http://forum.sagittal.org/viewtopic.php?p=3062#p3062
+
 const parseCommaName = (commaNameIo: Io): ParsedCommaName => {
     const quotientPartOfCommaName = commaNameIo // *not* a 2,3-free class, because it's not necessarily super!!!
         .replace(/[a-zA-Z+\-]/g, BLANK) as Io
