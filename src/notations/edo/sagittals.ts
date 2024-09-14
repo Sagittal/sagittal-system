858692eb2f99db92e6f3f0ba2b4458181e1b8c58
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2024-09-14 10:29:33 -0700

    only extend sagittals list if it's Revo, not if it's not Evo, because then it could happen for the new third flavor, Evo-SZ

--- src/notations/edo/sagittals.ts
+++ src/notations/edo/sagittals.ts
@@ -1,46 +1,56 @@
 import { Count, isEven } from "@sagittal/general"
-import { EdoStep } from "./types";
-import { computeApotomeComplement, Flavor, parseSagitype, Sagittal, Sagitype, Shafts } from "../../accidental";
-import { SAGITTAL_SHARP } from "../../accidental/sagittal/constants";
+import { EdoStep } from "./types"
+import {
+    computeApotomeComplement,
+    Flavor,
+    parseSagitype,
+    Sagittal,
+    Sagitype,
+    Shafts,
+} from "../../accidental"
+import { SAGITTAL_SHARP } from "../../accidental/sagittal/constants"
 
-const computeRequiredRevoSagittalCount = (sharpStep: EdoStep): Count<Sagittal> => isEven(sharpStep) ?
-    Math.floor(sharpStep / 2) - 1 as Count<Sagittal> :
-    Math.floor(sharpStep / 2) as Count<Sagittal>
+const computeRequiredRevoSagittalCount = (
+    sharpStep: EdoStep,
+): Count<Sagittal> =>
+    isEven(sharpStep)
+        ? ((Math.floor(sharpStep / 2) - 1) as Count<Sagittal>)
+        : (Math.floor(sharpStep / 2) as Count<Sagittal>)
 
 const shiftedSagittal = (sagittal: Sagittal): Sagittal => ({
     ...sagittal,
-    shafts: sagittal.shafts === Shafts.SINGLE ?
-        Shafts.TRIPLE :
-        Shafts.EX
+    shafts: sagittal.shafts === Shafts.SINGLE ? Shafts.TRIPLE : Shafts.EX,
 })
 
-const computeSagittals = (
-    { sagitypes, flavor, sharpStep }: { sagitypes: Sagitype[], flavor: Flavor, sharpStep: EdoStep }
-): Sagittal[] => {
+const computeSagittals = ({
+    sagitypes,
+    flavor,
+    sharpStep,
+}: {
+    sagitypes: Sagitype[]
+    flavor: Flavor
+    sharpStep: EdoStep
+}): Sagittal[] => {
     let sagittals: Sagittal[] = sagitypes.map(parseSagitype)
 
-    if (flavor === Flavor.EVO) {
-        return sagittals
-    }
+    if (flavor === Flavor.REVO) {
+        sagittals
+            .slice(0, computeRequiredRevoSagittalCount(sharpStep))
+            .reverse()
+            .forEach((sagittal: Sagittal): void => {
+                sagittals.push(
+                    computeApotomeComplement(sagittal) as Sagittal<Flavor.REVO>,
+                )
+            })
 
-    sagittals
-        .slice(0, computeRequiredRevoSagittalCount(sharpStep))
-        .reverse()
-        .forEach((sagittal: Sagittal): void => {
-            sagittals.push(computeApotomeComplement(sagittal) as Sagittal<Flavor.REVO>)
-        })
+        sagittals.push(SAGITTAL_SHARP)
 
-    sagittals.push(SAGITTAL_SHARP)
-
-    sagittals
-        .slice()
-        .forEach((sagittal: Sagittal): void => {
+        sagittals.slice().forEach((sagittal: Sagittal): void => {
             sagittals.push(shiftedSagittal(sagittal))
         })
+    }
 
     return sagittals
 }
 
-export {
-    computeSagittals,
-}
+export { computeSagittals }
