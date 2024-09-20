c3d1f11b9313291320c8956eb81676d9da3284ef
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2024-09-19 21:39:09 -0700

    1.0.71

--- src/notations/edo/subset.ts
+++ src/notations/edo/subset.ts
@@ -1,7 +1,7 @@
 import { Edo, SubsetEdoNotationDefinition, EdoNotationDefinition, SubsetFactor } from "./types"
 
 const isSubsetNotation = (edoNotationDefinition: EdoNotationDefinition): edoNotationDefinition is SubsetEdoNotationDefinition =>
-    (<SubsetEdoNotationDefinition>edoNotationDefinition).supersetEdo !== undefined
+    (<SubsetEdoNotationDefinition>edoNotationDefinition).supersetEdoName !== undefined
 
 const computeSubsetFactor = ({ edo, supersetEdo }: { edo: Edo, supersetEdo: Edo }): SubsetFactor => supersetEdo / edo as SubsetFactor
 
