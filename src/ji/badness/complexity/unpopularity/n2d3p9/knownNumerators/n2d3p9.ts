d6b12e34d208b0f9cf1d86c033d4fe0cb2755dc6
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2024-11-10 15:47:25 -0800

    negate

--- src/ji/badness/complexity/unpopularity/n2d3p9/knownNumerators/n2d3p9.ts
+++ src/ji/badness/complexity/unpopularity/n2d3p9/knownNumerators/n2d3p9.ts
@@ -1,4 +1,4 @@
-import { max } from "@sagittal/general"
+import { max, negate } from "@sagittal/general"
 import { N2D3P9 } from "../types"
 import { KnownLowN2D3P9Numerator } from "./types"
 
@@ -8,8 +8,8 @@ const computeN2D3P9FromKnownLowN2D3P9NumeratorAndPossibleDenominator = (
 ): N2D3P9 => {
     return ((knownNumerator.numerator *
         possibleDenominator.numerator *
-        2 ** -(knownNumerator.copfr as number) *
-        3 ** -(possibleDenominator.copfr as number) *
+        2 ** negate(knownNumerator.copfr) *
+        3 ** negate(possibleDenominator.copfr) *
         max(knownNumerator.gpf, possibleDenominator.gpf)) /
         9) as N2D3P9
 }
